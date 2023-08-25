7351882af84779e36fc350109f327fd2e98645b8
icloudfortnite <143171539+icloudfortnite@users.noreply.github.com>
2023-08-25 04:59:06 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+    if (typeof(Storage) !== "undefined") {
+        // Initialize the variable 'ipa' with 'false' if not already set
+        if (!localStorage.getItem('ipa')) {
+            localStorage.setItem('ipa', 'false');
+        }
+        
+        // Function to set 'ipa' to 'true'
+        function setIpaTrue() {
+            localStorage.setItem('ipa', 'true');
+        }
+        
+        // Function to retrieve the value of 'ipa'
+        function getIpaValue() {
+            return localStorage.getItem('ipa');
+        }
+    } else {
+        // Browser doesn't support local storage
+        console.log("Local storage is not supported by this browser.");
+    }
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
